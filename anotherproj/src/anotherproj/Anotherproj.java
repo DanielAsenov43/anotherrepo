a3868d87b8d326c4e9afd12b3efff4bb42153724
Daniel Asénov <dasenovp01@educantabria.es>
2025-01-28 10:25:11 +0100

    dhjask

--- anotherproj/src/anotherproj/Anotherproj.java
+++ anotherproj/src/anotherproj/Anotherproj.java
@@ -5,7 +5,7 @@ public class Anotherproj {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Test branch!");
+        System.out.println("Merged!");
     }
     
 }
